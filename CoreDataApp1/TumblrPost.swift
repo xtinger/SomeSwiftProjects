108f6c076cb184d6ec41b5ea104bfa8f00bad9f3
xtinger <xtinger@bk.ru>
2018-07-27 19:48:43 +0300

    first

---
+++ CoreDataApp1/TumblrPost.swift
@@ -0,0 +1,63 @@
+//
+//  Kitten.swift
+//  CoreDataApp1
+//
+//  Created by Denis Voronov on 27/07/2018.
+//  Copyright © 2018 EvergreenBits. All rights reserved.
+//
+
+import Foundation
+
+struct TumblrRoot : Codable {
+    let meta: TumblrMeta
+    let response: TumblrResponse
+}
+
+struct TumblrMeta : Codable {
+    let msg: String
+    let status: Int
+}
+
+struct TumblrResponse : Codable {
+    let blog: TumblrBlog
+    let posts: [TumblrPost]
+}
+
+struct TumblrBlog : Codable {
+    let name: String
+    let posts: Int
+    let title: String
+    let total_posts: Int
+    let url: URL
+}
+
+enum TumblrPostType : String, Codable{
+    case Post
+}
+
+struct TumblrPost : Codable {
+    let id: Int64
+    let type: TumblrPostType
+    let blog_name: String
+    let post_url: URL
+    let slug: String
+    let summary: String
+    let post_author: String
+    let source_url: URL
+    let source_title: String
+    let caption: String
+    let image_permalink: URL
+    let photos: [TumblrPhoto]
+}
+
+struct TumblrPhoto : Codable {
+    let caption: String
+    let original_size: TumblrPhotoSize
+    let alt_sizes: [TumblrPhotoSize]
+}
+
+struct TumblrPhotoSize : Codable {
+    let url: URL
+    let width: Int
+    let height: Int
+}
---
+++ CoreDataApp1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataApp1
+//
+//  Created by Denis Voronov on 27/07/2018.
+//  Copyright © 2018 EvergreenBits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
